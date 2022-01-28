cefae6f9e981ae00f4d59ba1a30c9c9b5279652c
Patryk Sandomierski <psandomierski@sii.pl>
2022-01-28 17:36:11 +0100

    Guess game

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # rust-sandbox
 ## running
-set env vars:
-```shell
-RUST_LOG=debug;RUST_LOG_STYLE=always
-```
-then run
+run
 ```shell
 run --package rust-sandbox --bin rust-sandbox
+```
+## offline docs
+```shell
+cargo doc --open
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
-use log::{info, debug};
+use std::cmp::Ordering;
+use std::io;
+
+use log::{info, warn};
 use log4rs;
+use rand::Rng;
 
 fn main() {
     log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
-    debug!("hiho");
-    info!("hmmmmm");
-    for i in 1..1000 {
-        info!(target: "log_file", "it should land in file only: {}", i);
+
+    let secret_num = rand::thread_rng().gen_range(1..101);
+    info!("guess game!");
+    info!("input your guess:");
+
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("failed to read line!");
+        let guess: u32 = match guess.trim().parse() { // parse return enum Result so match works
+            Ok(n) => n,
+            Err(_) => {
+                warn!("NaN! type valid number:");
+                continue;
+            }
+        };
+
+        info!("your type is: {}", guess);
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => info!("too small. try again:"),
+            Ordering::Equal => {
+                info!("bulls eye!");
+                break;
+            }
+            Ordering::Greater => info!("too big. try again:")
+        }
     }
 }
