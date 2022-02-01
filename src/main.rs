15aaf3bc4224dba8d8b87a9419bd7c6609a556d3
Patryk Sandomierski <psandomierski@sii.pl>
2022-02-01 23:24:43 +0100

    More structs, methods and constructor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn update_width(&mut self, width: u32) {
+        self.width = width
+    }
+}
+
 fn main() {
-    let rect1 = Rectangle {
+    let mut rect1 = Rectangle {
         width: 10,
         height: 15,
     };
+    dbg!(&rect1);
     println!("rect area={}", area(&rect1));
+    println!("rect area using impl={}", rect1.area());
+    rect1.update_width(5);
+    println!("rect area using impl={}", rect1.area());
+
+    let rect2 = Rectangle::square(5);
+    println!("square with side's length={} has area={}", rect2.height, rect2.area());
 }
 
 fn area(rect: &Rectangle) -> u32 {
