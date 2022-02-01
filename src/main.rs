dd81efd97851c203fd1c39966bbdf16378659203
Patryk Sandomierski <psandomierski@sii.pl>
2022-02-01 19:37:46 +0100

    Named loop awesomeness

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
-use std::cmp::Ordering;
-use std::io;
-
-use log::{info, warn};
+use log::{info};
 use log4rs;
-use rand::Rng;
 
 fn main() {
     log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
     let x = 3;
-    info!("let me add five to x={}: {}",x, add_five(x))
+    info!("let me add five to x={}: {}",x, add_five(x));
+
+    let be_five = true;
+    let number = if be_five { 5 } else { 4 };
+    info!("are we five: {} then we are: {}", be_five, number);
+    counting_loop_break();
 }
 
 fn add_five(x: i32) -> i32 {
     x + 5 // instead return, use statement without `;`
+}
+
+fn counting_loop_break() {
+    let mut count = 0;
+    'counting_up: loop { //awesome :D
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {}", count);
 }
