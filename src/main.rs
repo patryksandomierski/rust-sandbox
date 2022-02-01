8f8ece5a29b52cf3240a7140e8b27fc797912fc8
Patryk Sandomierski <psandomierski@sii.pl>
2022-02-01 19:27:23 +0100

    Statements and return values

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,10 @@ use rand::Rng;
 
 fn main() {
     log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
+    let x = 3;
+    info!("let me add five to x={}: {}",x, add_five(x))
+}
 
-    let secret_num = rand::thread_rng().gen_range(1..101);
-    info!("guess game!");
-    info!("input your guess:");
-
-    loop {
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("failed to read line!");
-        let guess: u32 = match guess.trim().parse() { // parse return enum Result so match works
-            Ok(n) => n,
-            Err(_) => {
-                warn!("NaN! type valid number:");
-                continue;
-            }
-        };
-
-        info!("your type is: {}", guess);
-
-        match guess.cmp(&secret_num) {
-            Ordering::Less => info!("too small. try again:"),
-            Ordering::Equal => {
-                info!("bulls eye!");
-                break;
-            }
-            Ordering::Greater => info!("too big. try again:")
-        }
-    }
+fn add_five(x: i32) -> i32 {
+    x + 5 // instead return, use statement without `;`
 }
